6bc4fe054ceffb6a19577fc157832c4d8218486c
Lukas Manus <lukas.manus@t-online.de>
2020-02-01 19:40:38 +0100

    Automatic commit before shutdown.

--- Lukas/thirdLesson/calculator.c
+++ Lukas/thirdLesson/calculator.c
@@ -1,21 +1,65 @@
 #include <stdio.h>
+#define CLEAR while(getchar()!='\n')
 
 int main(void){
     
-    //Getting first input.
-    int firstNumberBool = 0, secondNumberBool = 0;
-    float firstNumber, secondNumber;
-    char calcSymbol;
+    int run = 1;
+    double validInput = 0, firstNumber, secondNumber;
+    char operationSign;
     
-    while(firstNumberBool==0){
-        printf("Bitte geben Sie die erste Zahl ein.\n");
-        if(scanf("%f", &firstNumber)==1){
-            firstNumberBool = 1;
+    //First run.
+    //User input.
+    while(validInput == 0){
+        printf("Bitte geben Sie eine Zahl ein.\n");
+        if((scanf("%lf", &firstNumber))==1){
+            validInput = 1;
+        }
+        CLEAR;
+    }
+    validInput = 0;
+    
+    while(run==1){
+        while(validInput == 0){
+            printf("Bitte geben Sie eine Rechenoperation(+, -, *, /) ein.\n");
+            if((scanf("%c", &operationSign))==1 && (operationSign == '+' || operationSign == '-' || operationSign == '*' || operationSign == '/')){
+                validInput = 1;
+            }
+            CLEAR;
+        }
+        validInput = 0;
+        
+        while(validInput == 0){
+            printf("Bitte geben Sie eine Zahl ein.\n");
+            if((scanf("%lf", &secondNumber))==1){
+                validInput = 1;
+            }
+            CLEAR;
+        }
+        validInput = 0;
+        
+        printf("Rechenzeichen ist: %c.\n", operationSign);
+        if(operationSign == '+'){
+            firstNumber = firstNumber + secondNumber;
+        }
+        else if(operationSign == '-'){
+            firstNumber = firstNumber - secondNumber;
+        }
+        else if(operationSign == '*'){
+            firstNumber = firstNumber * secondNumber;
         }
         else{
-            firstNumberBool = 0;
+            firstNumber = firstNumber / secondNumber;
         }
-        fflush(stdin);
-    }
+        
+        printf("Das Ergebnis ist: %lf.\n", firstNumber);
     
+        while(validInput == 0){
+            printf("Möchten Sie eine weitere Berechnung durchführen?\nWenn ja, geben Sie eine 1 ein.\nWenn nein, geben Sie eine 0 ein.\n");
+            if((scanf("%d", &run) == 1 && (run == 0 || run == 1))){
+                validInput = 1;
+            }
+            CLEAR;
+        }
+        validInput = 0;
+    }
 }
